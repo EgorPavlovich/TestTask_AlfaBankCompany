61df6154e6b3ba2b786458ee291bf56fb8ac79d1
Egor Pavlovich <ambidexterdexter@gmail.com>
2020-11-18 19:26:03 +0300

    #Console-002 Добавлен метод ConvertRtfToTxt
    Метод ConvertRtfToTxt нужен для конвертации содержимого исходного .rtf файла в обычный текстовый формат и сохранения результата в выходной файл

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,39 @@ namespace ConsoleApp
 {
     class Program
     {
+        private static string OriginalRTFfile = @"testDoc.rtf";
+        private static string OutputTxtfile = @"Output file.txt";
+
         static void Main(string[] args)
         {
+            ConvertRtfToTxt(OriginalRTFfile);
+
+            Console.ReadLine();
         }
+
+        public static void ConvertRtfToTxt(string path_)
+        {
+            string path = path_;
+            System.Windows.Forms.RichTextBox rtBox = new System.Windows.Forms.RichTextBox();
+            string rtfText = System.IO.File.ReadAllText(path);
+            rtBox.Rtf = rtfText;
+            string plainText = rtBox.Text;
+
+            using (StreamWriter file = new StreamWriter(OutputTxtfile, true, System.Text.Encoding.Default))
+            {
+                file.WriteLine(plainText);
+            }
+
+            // читаем все данные из файла
+            string[] lines = System.IO.File.ReadAllLines(OutputTxtfile, Encoding.Default);
+            // преобразуем в список
+            var list = new List<string>(lines);
+            // получаем только уникальные элементы
+            var uniqueStrings = list.Distinct();
+            // записываем их обратно                      
+            System.IO.File.WriteAllLines(OutputTxtfile, uniqueStrings, System.Text.Encoding.Default);
+        }
+
+
     }
 }
