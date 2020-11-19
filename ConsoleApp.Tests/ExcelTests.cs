560bd15118ff6d26820be7f841b72effd650be73
Egor Pavlovich <ambidexterdexter@gmail.com>
2020-11-20 02:47:53 +0300

    #Tests-001 Добавлен проект для Unit тестирования консольного приложения
    Создан модульный тест для метода
    CreateQueryByTask из файла QueryByTask.cs
    
    Создан модульный тест для метода
    AddTableSheet из файла Excel.cs

---
+++ ConsoleApp.Tests/ExcelTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Tests
+{
+    [TestClass()]
+    public class ExcelTests
+    {
+        [TestMethod()]
+        public void AddTableSheetTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
